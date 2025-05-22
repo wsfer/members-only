56874d3ea806cae9a6a78578a2092bd8f0469be1
wsfer <93054415+wsfer@users.noreply.github.com>
2025-05-22 16:23:34 -0300

    feat: create script to populate database

---
+++ database/populatedb.js
@@ -0,0 +1,35 @@
+// Usage: node database/populatedb.js <CONNECTION_STRING>
+const { Client } = require('pg');
+const { argv } = require('node:process');
+
+const CONNECTION_STRING = argv[2];
+
+const SQL = `
+  CREATE TABLE IF NOT EXISTS account (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    is_member BOOLEAN DEFAULT FALSE,
+    is_admin BOOLEAN DEFAULT FALSE
+  );
+
+  CREATE TABLE IF NOT EXISTS post (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER REFERENCES account(id) ON DELETE CASCADE
+  );
+`;
+
+async function main() {
+  console.log('Setting database...');
+  const client = new Client({ connectionString: CONNECTION_STRING });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('Done');
+}
+
+main();
