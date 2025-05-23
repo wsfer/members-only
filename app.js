997cd42da3ec4585d2a6f8ca2d228a16e1e5b4df
wsfer <93054415+wsfer@users.noreply.github.com>
2025-05-23 16:34:18 -0300

    feat: add session and authentication middlewares to app

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const app = express();
+const passport = require('./config/passport');
+const session = require('./middlewares/session.middleware');
 const userRouter = require('./routes/user.routes');
 const postRouter = require('./routes/post.routes');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session);
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/user', userRouter);
 app.use('/', postRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 const userController = require('../controllers/user.controller');
+const passport = require('../config/passport');
 
 const userRouter = Router();
 
 userRouter.get('/login', userController.getLoginForm);
 userRouter.get('/register', userController.getRegisterForm);
-userRouter.post('/login', (req, res) => res.redirect('/')); // Passport.js should be here
+userRouter.post(
+  '/login',
+  passport.authenticate('local', { successRedirect: '/' })
+);
 userRouter.post('/register', userController.createUser);
 
 module.exports = userRouter;
