534d37d1614b93e6363a8d894b951a4b8bdc27db
wsfer <93054415+wsfer@users.noreply.github.com>
2025-05-24 16:36:11 -0300

    feat: add support for session secret from env variables

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -3,12 +3,13 @@ const expressSession = require('express-session');
 const crypto = require('crypto');
 const pgSession = require('connect-pg-simple')(expressSession);
 
-const randomSecret = crypto.randomBytes(48).toString('hex');
 const store = new pgSession({ pool: pool, createTableIfMissing: true });
+const sessionSecret =
+  process.env.SESSION_SECRET || crypto.randomBytes(48).toString('hex');
 
 module.exports = expressSession({
   store: store,
-  secret: randomSecret,
+  secret: sessionSecret,
   resave: false,
   saveUninitialized: false,
   cookie: {
