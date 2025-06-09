6c5363d472658bc2312ea9291577f84a8fd31455
wsfer <93054415+wsfer@users.noreply.github.com>
2025-06-09 16:08:10 -0300

    refactor: move images and scripts to it's own directories on public folder

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,6 @@ module.exports = defineConfig([
   },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-  globalIgnores(['public/*.js']),
+  globalIgnores(['public/scripts/*.js']),
   eslintConfigPrettier,
 ]);
