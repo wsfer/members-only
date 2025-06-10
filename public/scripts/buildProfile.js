3f841b7216aadfc6b872061bd363f9bbd2eb8a86
wsfer <93054415+wsfer@users.noreply.github.com>
2025-06-10 15:43:01 -0300

    feat: add emoji and color inputs for user profile on register page

---
+++ public/scripts/buildProfile.js
@@ -0,0 +1,23 @@
+// TODO: generate a random emoji and color on first visit
+(() => {
+  // Inputs
+  const emojiPicker = document.querySelector('.js-emoji-picker');
+  const emojiHiddenInput = document.querySelector('.js-emoji-input');
+  const colorInput = document.querySelector('.js-color-input');
+
+  // Profile preview
+  const profileColor = document.querySelector('.js-profile-color');
+  const profileEmoji = document.querySelector('.js-profile-emoji');
+
+  emojiPicker.addEventListener('emoji-click', (e) => {
+    emojiHiddenInput.value = e.detail.unicode;
+    profileEmoji.textContent = e.detail.unicode;
+  });
+
+  colorInput.addEventListener('input', (e) => {
+    profileColor.style.backgroundColor = e.target.value;
+  });
+
+  profileEmoji.textContent = emojiHiddenInput.value || '😀';
+  profileColor.style.backgroundColor = colorInput.value;
+})();
