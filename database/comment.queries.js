eb217a39803034a0f3ba2a485981118d496b83ed
wsfer <93054415+wsfer@users.noreply.github.com>
2025-06-19 15:02:39 -0300

    feat: create query to get comments by post id

--- database/comment.queries.js
+++ database/comment.queries.js
@@ -1,5 +1,15 @@
 const pool = require('../config/pool');
 
+async function getByPost(postId) {
+  const { rows } = await pool.query(
+    `SELECT * FROM comment JOIN account
+    ON comment.account_id = account.account_id
+    WHERE post_id = $1`,
+    [postId]
+  );
+  return rows;
+}
+
 async function createComment({ postId, userId, message }) {
   await pool.query(
     `INSERT INTO comment (post_id, account_id, message) VALUES ($1, $2, $3)`,
@@ -7,4 +17,4 @@ async function createComment({ postId, userId, message }) {
   );
 }
 
-module.exports = { createComment };
+module.exports = { getByPost, createComment };
