22730e23c7a7d2eb15de8850d6a13a33f5dff46f
wsfer <93054415+wsfer@users.noreply.github.com>
2025-06-19 14:50:08 -0300

    feat: add post comment routes and form feedback

--- routes/comment.router.js
+++ routes/comment.router.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const commentController = require('../controllers/comment.controller');
+const auth = require('../middlewares/auth.middleware');
 const NotFoundError = require('../errors/NotFoundError');
 
 const commentRouter = Router();
 
+commentRouter.post('/:id', auth.isLoggedIn, commentController.createComment);
 commentRouter.use(() => {
   throw new NotFoundError('Content not found');
 });
