7aa05329bab688a13d4a739e9a3041527b13e65a
wsfer <93054415+wsfer@users.noreply.github.com>
2025-06-19 14:48:41 -0300

    feat: add query to create a comment

--- database/comment.queries.js
+++ database/comment.queries.js
@@ -1,3 +1,10 @@
 const pool = require('../config/pool');
 
-module.exports = {};
+async function createComment({ postId, userId, message }) {
+  await pool.query(
+    `INSERT INTO comment (post_id, account_id, message) VALUES ($1, $2, $3)`,
+    [postId, userId, message]
+  );
+}
+
+module.exports = { createComment };
